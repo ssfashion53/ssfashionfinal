d680fac697d6005a780f8d6ab2e2136492c487ae
ssfashion53 <ssfashion051@gmail.com>
2024-12-16 20:19:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript code can be used for additional functionalities
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('SS Fashion Shop business card loaded successfully!');
+});
